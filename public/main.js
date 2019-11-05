0493476bccd14195fc40614e436409f0c69afe9e
Justin Swiger <justin.swiger@me.com>
2019-11-05 11:57:59 -0500

    finalized

--- public/main.js
+++ public/main.js
@@ -1,7 +1,34 @@
+// TODO: on page load, generate a random number
+// TODO: when user clicks button, we check if input equals the generated number
+// TODO: if user guesses correct number, display message
+// TODO: if user guesses incorrect number, display message
+const qs = (element) => document.querySelector(element)
+
+let randomNumber = 0
+
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+  randomNumber = Math.ceil(Math.random() * 10)
+  console.log(randomNumber)
+}
+
+const checkTheNumbers = () => {
+  // get the number from the HTML input
+  // parseInt (turned string to number)
+  const userGuess = parseInt(qs('input').value)
+  console.log(userGuess)
+  console.log(typeof userGuess)
+  console.log(typeof randomNumber)
+  // check if the number === randomNumber
+  if (randomNumber === userGuess) {
+    // if yes, show success message
+    console.log('success')
+    qs('.output-message').textContent = 'Success!'
+  } else {
+    // if no (else), show try again message
+    console.log('nope')
+    qs('.output-message').textContent = 'Nope. Try again!'
   }
 }
 
 document.addEventListener('DOMContentLoaded', main)
+qs('button').addEventListener('click', checkTheNumbers)
