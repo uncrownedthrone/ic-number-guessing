73287a0fc19be82c89cfcefe726e6e910091542e
Justin Swiger <justin.swiger@me.com>
2019-11-06 13:10:53 -0500

    fixed js

--- public/main.js
+++ public/main.js
@@ -1,69 +1,78 @@
 // DONE: on page load, generate a random number
-// DONE: when user clicks button, we check if input equals the generated number
-// DONE: if user guesses correct number, display message
-// DONE: if user guesses incorrect number, display message
+// DONE: when the user clicks the button, we check if the inputted number === generated number
+// DONE: if the user guessed the correct number, display message of success
 
-// DONE: add a counter to see how many guesses
-// DONE: limit number of guesses to 7
-// DONE: add a too high/too low message
+// DONE: Add a counter to count the number of times guessed
+// DONE: limit the number of guesses to 7
+// DONE: Add a message for too high/too low
 
-// TODO: add a message if user guesses picked number
-
-const qs = (e) => document.querySelector(e)
+// TODO: Add a message if a user has guessed the number already
 
 let randomNumber = 0
 let numberOfGuesses = 0
-let guessedNumbers = []
+const guessedNumbers = []
 
 const main = () => {
   randomNumber = Math.ceil(Math.random() * 10)
   console.log(randomNumber)
 }
 
-// param: the number to check
-// return: true of false
-const hasUserGuessedNumber = (numberUserHasGuess) => {
+// param: the number checkout
+// return value: true/false if the user has guessed the number
+
+const hasUserGuessedTheNumber = (userGuess) => {
+  console.log('checking is the user guess ', userGuess)
   for (let i = 0; i < guessedNumbers.length; i++) {
-    if (guessedNumbers[i] === numberUserHasGuess) {
+    if (guessedNumbers[i] === userGuess) {
       return true
     }
   }
   return false
 }
 
+const showHighLowMessage = (userGuess) => {
+  if (randomNumber > userGuess) {
+    document.querySelector('.output-message').classList.add('too-low')
+    document.querySelector('.output-message').classList.remove('too-high')
+
+    document.querySelector('.output-message').textContent =
+      'Try Again with a bigger number!'
+  } else if (randomNumber < userGuess) {
+    document.querySelector('.output-message').textContent =
+      'Try Again with a smaller number!'
+    document.querySelector('.output-message').classList.add('too-high')
+    document.querySelector('.output-message').classList.remove('too-low')
+  }
+}
+
 const checkTheNumbers = () => {
   // get the number from the HTML input
-  // parseInt (turned string to number)
-  const userGuess = parseInt(qs('input').value)
+  const userGuess = parseInt(document.querySelector('input').value)
   numberOfGuesses++
-  hasUserGuessedNumber(userGuess)
-  guessedNumbers.push(userGuess)
-  console.log(guessedNumbers)
-  // check if the number === randomNumber
-  if (randomNumber === userGuess && numberOfGuesses <= 7) {
-    // if yes, show success message
-    qs('.output-message').textContent =
-      'Success! You guessed the number in ' + numberOfGuesses + ' tries!'
-  } else if (numberOfGuesses >= 7) {
-    qs('.output-message').textContent =
-      'Sorry, you guessed too many times. Play again?'
-    qs('button').disabled = true
+
+  const hasGuessed = hasUserGuessedTheNumber(userGuess)
+
+  if (hasGuessed) {
+    document.querySelector('.output-message').textContent =
+      'Wait, you have guessed that already!'
+    showHighLowMessage(userGuess)
   } else {
-    // if no (else), show try again message
-    if (randomNumber > userGuess) {
-      qs('.output-message').classList.add('too-low')
-      qs('.output-message').classList.remove('too-high')
-      qs('.output-message').textContent =
-        'Nope. Try again with a bigger number.'
-    } else if (randomNumber < userGuess) {
-      qs('.output-message').textContent =
-        'Nope. Try again with a smaller number.'
-      qs('.output-message').classList.add('too-high')
-      qs('.output-message').classList.remove('too-low')
+    guessedNumbers.push(userGuess)
+    console.log(guessedNumbers)
+    // check if the number === randomNumber
+    if (randomNumber === userGuess && numberOfGuesses <= 7) {
+      // if yes, show success message
+      document.querySelector('.output-message').textContent =
+        'Success! You guessed the number in ' + numberOfGuesses + ' tries'
+    } else if (numberOfGuesses >= 7) {
+      document.querySelector('.output-message').textContent =
+        'Sorry, you have guessed too many times. Play again?'
+      document.querySelector('button').disabled = true
+    } else {
+      showHighLowMessage(userGuess)
     }
   }
-  qs('.output-message').textContent = 'Nope. Try again!'
 }
 
 document.addEventListener('DOMContentLoaded', main)
-qs('button').addEventListener('click', checkTheNumbers)
+document.querySelector('button').addEventListener('click', checkTheNumbers)
